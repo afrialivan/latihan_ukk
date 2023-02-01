efcec907317502e1b38962ee12fae2f0ad78b936
afrialivan <afrialivan0@gmail.com>
2023-02-01 21:56:08 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<a href="/pengaduan">buat laporan</a>
+
+{{ auth()->user()->nama }}
+
+
+<br><br><br>
+<table>
+    @foreach ($reports as $report)
+    <tr>
+        <td>{{ $report['tgl_pengaduan'] }}</td>
+        <td>{{ $report['isi_laporan']}}</td>
+        <td>{{ $report['nik']}}</td>
+    </tr>
+    @endforeach
+</table>
